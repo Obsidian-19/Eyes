9a3aa6267b319fbfdbecbee7df3328c32a8249c0
Erik Bacchus <90561191+Obsidian-19@users.noreply.github.com>
2021-12-13 23:45:54 -0500

    Add files via upload

---
+++ eyes.js
@@ -0,0 +1,12 @@
+const ball = document.getElementsByClassName('ball');
+
+document.onmousemove = (event) => {
+  const x = (event.clientX * 100) / window.innerWidth + '%';
+  const y = (event.clientY * 100) / window.innerHeight + '%';
+
+for (let i = 0; i < 2; i++) {
+  ball[i].style.left = x;
+  ball[i].style.top = y;
+  ball[i].transform = 'translate(-' + x + ',-' + y + ')';
+  }
+};
